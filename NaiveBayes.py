0695474b332aa1058913bf55ab14ab42f2d066d5
kairungIT <113651426+kairungIT@users.noreply.github.com>
2025-03-18 11:26:58 +0700

    Update NaiveBayes.py

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -42,5 +42,5 @@ st.write(f"ท่านเป็นโรคหัวใจหรือไม่
 
 # แสดงความน่าจะเป็นของแต่ละคลาส
 st.subheader("ความน่าจะเป็นของแต่ละประเภท:")
-df_proba = pd.DataFrame(prediction_proba, columns=Heart.HeartDisease)
+df_proba = pd.DataFrame(prediction_proba, columns='HeartDisease')
 st.dataframe(df_proba.style.format("{:.2%}"))
